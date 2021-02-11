616e22afdb12d9a3f8ca30a34fe7d486723b8cd0
guiireal <guii_real@hotmail.com>
2021-02-10 23:28:10 -0300

    feat: test password

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -43,6 +43,7 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
+
   test("Should return 400 if no password confirmation is provider", () => {
     const sut = new SignUpController();
     const httpRequest = {
